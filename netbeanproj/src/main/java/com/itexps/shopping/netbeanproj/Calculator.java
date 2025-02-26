a4e18348d79601f79dbf2410716e68f226a19e3d
sugitha92 <sugitha.palaniselvam@gmail.com>
2025-02-25 20:53:33 -0600

    initial project

---
+++ netbeanproj/src/main/java/com/itexps/shopping/netbeanproj/Calculator.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.itexps.shopping.netbeanproj;
+
+/**
+ *
+ * @author sugit
+ */
+public class Calculator {
+    
+    public int add(int x, int y)
+    {
+        return x + y;
+    }
+    
+    public int mul(int x, int y){
+        return x * y;
+    }
+}
